2884c0b904f52cef2bb3f3cadae3eca53c17a4ed
Vadzim Zieńka <v@vadzim.info>
2019-10-22 11:58:56 +0300

    dummy test for coverage

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+// @flow
+
+import "../src/index"
+
+declare var describe: Function
+declare var it: Function
+
+describe("stub for checking coverage", () => { it("shouldn't do anything", () => {}) })
