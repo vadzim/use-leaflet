07d438faa67397e3f5c1fe1550742f68c5dba52d
Vadzim Zieńka <v@vadzim.info>
2019-11-18 23:40:32 +0300

    fix: use `undefined` type instead of `void` in ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
 import { Map } from "leaflet"
 
 export function useLeafletZoom(): number
-export function useLeafletMap(): Map | void
+export function useLeafletMap(): Map | undefined
 export function useLeafletBounds(): [[number, number], [number, number]]
 export function useLeafletCenter(): [number, number]
 export function useLeafletIsMoving(): boolean
